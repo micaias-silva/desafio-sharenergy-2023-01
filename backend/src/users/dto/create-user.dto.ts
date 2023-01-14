c3253eab394f531163a937ae627222ef06b87571
Micaías Silva <micaiasilvaop@gmail.com>
2023-01-14 18:36:57 -0300

    feat: adicionadas novas propriedades ao DTO de usuário para equivaler ao user schema

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { Exclude, Expose } from 'class-transformer';
+
 export class CreateUserDto {
   username: string;
   password: string;
+  email: string;
+  firstName?: string;
+  lastName?: string;
+  profilePictureUrl?: string;
+  birthdate?: Date;
 }
