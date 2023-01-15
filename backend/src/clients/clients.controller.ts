d1bbcf0e195318bf9e0fb12ede692841b4f45f5d
Micaías Silva <micaiasilvaop@gmail.com>
2023-01-15 16:03:28 -0300

    feat: tratativa de erros

--- backend/src/clients/clients.controller.ts
+++ backend/src/clients/clients.controller.ts
@@ -24,45 +24,39 @@ import { ClientSerializer } from './serializers/client.serializer';
 
 @SerializeOptions({ excludeExtraneousValues: true })
 @UseInterceptors(ClassSerializerInterceptor)
+@Roles(Role.Admin)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('clients')
 export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
 
   @Post()
-  @Roles(Role.Admin)
-  @UseGuards(JwtAuthGuard, RolesGuard)
   async create(@Body() data: CreateClientDto) {
     const user = await this.clientsService.create(data);
     return new ClientSerializer(user);
   }
 
   @Get()
-  @UseGuards(JwtAuthGuard)
   async findAll() {
     const usersPage = await this.clientsService.findAll();
     return new PaginatedClientSerializer(usersPage);
   }
 
   @Get(':id')
-  @UseGuards(JwtAuthGuard)
   async findOne(@Param('id') id: string) {
     const client = await this.clientsService.findOne(id);
-    return new ClientSerializer(client!);
+    return new ClientSerializer(client);
   }
 
   @Patch(':id')
-  @Roles(Role.Admin)
-  @UseGuards(JwtAuthGuard, RolesGuard)
   async update(
     @Param('id') id: string,
     @Body() updateClientDto: UpdateClientDto,
   ) {
-    await this.clientsService.update(id, updateClientDto);
+    return await this.clientsService.update(id, updateClientDto);
   }
 
   @Delete(':id')
-  @Roles(Role.Admin)
-  @UseGuards(JwtAuthGuard, RolesGuard)
   async remove(@Param('id') id: string) {
     await this.clientsService.remove(id);
   }
