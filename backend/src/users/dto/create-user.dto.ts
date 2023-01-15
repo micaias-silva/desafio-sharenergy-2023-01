be10c63f804a3bde9f2b4b9f397456ab116f16fe
Micaías Silva <micaiasilvaop@gmail.com>
2023-01-14 21:57:19 -0300

    feat: marcações de validação para o DTO de usuários

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,11 +1,53 @@
-import { Exclude, Expose } from 'class-transformer';
+import { Transform } from 'class-transformer';
+import {
+  IsDate,
+  IsDateString,
+  IsDefined,
+  IsEmail,
+  IsLowercase,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
+  @IsString()
+  @IsDefined()
+  @MinLength(3)
+  @MaxLength(20)
+  @IsLowercase()
+  @Matches(/^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/)
   username: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(6)
+  @MaxLength(64)
   password: string;
+
+  @IsString()
+  @IsDefined()
+  @IsEmail()
   email: string;
+
+  @IsOptional()
+  @MinLength(4)
+  @MaxLength(32)
   firstName?: string;
+
+  @IsOptional()
+  @MinLength(4)
+  @MaxLength(32)
   lastName?: string;
+
+  @IsOptional()
+  @IsUrl()
   profilePictureUrl?: string;
+
+  @IsOptional()
+  @IsDateString({})
   birthdate?: Date;
 }
