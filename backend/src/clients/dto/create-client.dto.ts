561a07a145c4dbba82ec21f15ece1e06de5cb537
Micaías Silva <micaiasilvaop@gmail.com>
2023-01-14 22:02:32 -0300

    feat: marcações de validação para o DTO de clientes

--- backend/src/clients/dto/create-client.dto.ts
+++ backend/src/clients/dto/create-client.dto.ts
@@ -1,9 +1,33 @@
+import {
+  IsDefined,
+  IsEmail,
+  IsPhoneNumber,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { Address } from 'src/schemas/address.schema';
 
 export class CreateClientDto {
+  @IsDefined()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(255)
   name: string;
+
+  @IsDefined()
+  @IsEmail()
   email: string;
-  phone_number: string;
+
+  @IsPhoneNumber('BR')
+  phoneNumber: string;
+
+  @IsString()
+  @Matches(
+    /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/,
+  )
   cpf: string;
+
   address: Address;
 }
