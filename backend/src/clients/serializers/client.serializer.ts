fa03929690065c65f57164637e0e3705c747fb20
Micaías Silva <micaiasilvaop@gmail.com>
2023-01-15 12:12:47 -0300

    feat: introduzidos serializers para clientes

---
+++ backend/src/clients/serializers/client.serializer.ts
@@ -0,0 +1,36 @@
+import { Expose, Transform, Type } from 'class-transformer';
+import { PaginateResult } from 'mongoose';
+import { ClientDocument } from 'src/schemas/client.schema';
+import { AddressSerializer } from './address.serializer';
+
+export class ClientSerializer {
+  @Expose()
+  @Transform(({ obj }) => obj._id.toString())
+  id: string;
+  @Expose()
+  name: string;
+
+  @Expose()
+  email: string;
+
+  @Expose()
+  phoneNumber: string;
+
+  @Expose()
+  cpf: string;
+
+  @Expose()
+  @Type(() => AddressSerializer)
+  address: AddressSerializer;
+
+  constructor(partial: Partial<any>) {
+    partial = partial.toObject({
+      getters: true,
+      virtuals: false,
+      minimize: true,
+    });
+
+    console.log(this.address);
+    Object.assign(this, partial);
+  }
+}
