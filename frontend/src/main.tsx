61bbe069ca0f29709ef33bd486b2e0406232ac9f
Micaías Silva <micaiasilvaop@gmail.com>
2023-01-16 13:27:46 -0300

    feat: provider do redux

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import store from './store';
+
+import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './styles/Global';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <GlobalStyle>
-        <App />
-      </GlobalStyle>
+      <Provider store={store}>
+        <GlobalStyle>
+          <App />
+        </GlobalStyle>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
