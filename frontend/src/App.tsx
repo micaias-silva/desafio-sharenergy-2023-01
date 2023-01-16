5766eb653668c9914b7df180b670362cdcb5b796
Micaías Silva <micaiasilvaop@gmail.com>
2023-01-16 09:10:43 -0300

    feat: configuração pós vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Route, Switch } from 'react-router-dom';
+import NavigationHeader from './components/NavigationHeader';
+import HomePage from './pages/HomePage';
+import HttpCatsPage from './pages/HttpCatsPage';
+import RandomDogPage from './pages/RandomDogPage';
+import { Wrapper } from './styles/Containers';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Wrapper>
+        <NavigationHeader />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/random-dog">
+            <RandomDogPage />
+          </Route>
+          <Route exact path="/http-cats">
+            <HttpCatsPage />
+          </Route>
+        </Switch>
+      </Wrapper>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
