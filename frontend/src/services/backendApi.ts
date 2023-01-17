b123e0ac50a33730e55f4dd4a6cdf4d667fe4178
Micaías Silva <micaiasilvaop@gmail.com>
2023-01-16 22:00:37 -0300

    feat: tipagem das responses retornadas pela api

--- frontend/src/services/backendApi.ts
+++ frontend/src/services/backendApi.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+export const backendApi = axios.create({ baseURL: 'http://localhost:3000/' });
+
 export interface BackendUserResponse {
   id: string;
   username: string;
@@ -10,4 +12,40 @@ export interface BackendUserResponse {
   profilePictureUrl?: string;
 }
 
-export const backendApi = axios.create({ baseURL: 'http://localhost:3000/' });
+export interface BackendClientResponse {
+  id: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  cpf: string;
+  address: {
+    id: string;
+    zipcode: string;
+    number: string;
+    country: string;
+    neighborhood?: string;
+    state?: string;
+    city?: string;
+    street?: string;
+  };
+}
+
+export interface PaginationResponse {
+  totalDocs: number;
+  limit: number;
+  totalPages: number;
+  page: number;
+  pagingCounter: number;
+  hasPrevPage: boolean;
+  hasNextPage: boolean;
+  prevPage: number | null;
+  nextPage: number | null;
+}
+
+export interface PaginatedUserResponse extends PaginationResponse {
+  docs: BackendUserResponse[];
+}
+
+export interface PaginatedClienResponse extends PaginationResponse {
+  docs: BackendClientResponse[];
+}
